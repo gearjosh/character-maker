1d108125008c2baf2d4dbde84b77e179a298b3af
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-07 15:10:54 -0700

    add, config jasmine & karma

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,10 @@ module.exports = {
       // },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
         loader: "eslint-loader",
         // options: {
         //   eslint options (if necessary)
